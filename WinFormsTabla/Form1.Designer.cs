3e542fd20f1418d0be741cf68733e492414a085b
Juan Antonio <jaalbacardona@gmail.com>
2024-11-24 00:39:32 -0600

    agregar el proyecto

---
+++ WinFormsTabla/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WinFormsTabla
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelprincipal = new Panel();
+            richTextBoxresultados = new RichTextBox();
+            buttonver = new Button();
+            textBoxnum = new TextBox();
+            labelnumero = new Label();
+            labeltitulo = new Label();
+            labelnombre = new Label();
+            panelprincipal.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelprincipal
+            // 
+            panelprincipal.BackColor = Color.FromArgb(192, 255, 192);
+            panelprincipal.Controls.Add(labelnombre);
+            panelprincipal.Controls.Add(richTextBoxresultados);
+            panelprincipal.Controls.Add(buttonver);
+            panelprincipal.Controls.Add(textBoxnum);
+            panelprincipal.Controls.Add(labelnumero);
+            panelprincipal.Controls.Add(labeltitulo);
+            panelprincipal.Location = new Point(15, 12);
+            panelprincipal.Name = "panelprincipal";
+            panelprincipal.Size = new Size(773, 426);
+            panelprincipal.TabIndex = 0;
+            // 
+            // richTextBoxresultados
+            // 
+            richTextBoxresultados.Font = new Font("Times New Roman", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            richTextBoxresultados.Location = new Point(410, 100);
+            richTextBoxresultados.Name = "richTextBoxresultados";
+            richTextBoxresultados.Size = new Size(337, 282);
+            richTextBoxresultados.TabIndex = 4;
+            richTextBoxresultados.Text = "";
+            // 
+            // buttonver
+            // 
+            buttonver.Location = new Point(125, 224);
+            buttonver.Name = "buttonver";
+            buttonver.Size = new Size(110, 42);
+            buttonver.TabIndex = 3;
+            buttonver.Text = "Ver tabla";
+            buttonver.UseVisualStyleBackColor = true;
+            buttonver.Click += buttonver_Click;
+            // 
+            // textBoxnum
+            // 
+            textBoxnum.Location = new Point(201, 172);
+            textBoxnum.Name = "textBoxnum";
+            textBoxnum.Size = new Size(112, 27);
+            textBoxnum.TabIndex = 2;
+            // 
+            // labelnumero
+            // 
+            labelnumero.AutoSize = true;
+            labelnumero.Font = new Font("Comic Sans MS", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            labelnumero.ForeColor = Color.DarkCyan;
+            labelnumero.Location = new Point(15, 172);
+            labelnumero.Name = "labelnumero";
+            labelnumero.Size = new Size(180, 25);
+            labelnumero.TabIndex = 1;
+            labelnumero.Text = "¿Cual tabla quieres?";
+            // 
+            // labeltitulo
+            // 
+            labeltitulo.AutoSize = true;
+            labeltitulo.Font = new Font("Papyrus", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
+            labeltitulo.ForeColor = Color.Indigo;
+            labeltitulo.Location = new Point(277, 22);
+            labeltitulo.Name = "labeltitulo";
+            labeltitulo.Size = new Size(243, 37);
+            labeltitulo.TabIndex = 0;
+            labeltitulo.Text = "Tabla de Multiplicar";
+            // 
+            // labelnombre
+            // 
+            labelnombre.AutoSize = true;
+            labelnombre.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            labelnombre.Location = new Point(15, 387);
+            labelnombre.Name = "labelnombre";
+            labelnombre.Size = new Size(381, 28);
+            labelnombre.TabIndex = 5;
+            labelnombre.Text = "Hecho por: Juan Antonio Alba Cardona";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panelprincipal);
+            Name = "Form1";
+            Text = "Form1";
+            panelprincipal.ResumeLayout(false);
+            panelprincipal.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelprincipal;
+        private Label labeltitulo;
+        private Label labelnumero;
+        private TextBox textBoxnum;
+        private Button buttonver;
+        private RichTextBox richTextBoxresultados;
+        private Label labelnombre;
+    }
+}
